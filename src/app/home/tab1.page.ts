354119834700e24cb8c02803aee1d01d9f648a18
Alexandre BG <alexandre.brungiglio@nextalys.com>
2021-11-05 15:36:41 +0100

    fix global routes & routing

--- src/app/home/tab1.page.ts
+++ src/app/home/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutesList } from '../../environments/routes';
 
 @Component({
   selector: 'app-tab1',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  routes = RoutesList;
 
   constructor() { }
 
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutesList } from '../../environments/routes';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-
-  constructor() {}
+  routes = RoutesList;
+  constructor() { }
 
 }
