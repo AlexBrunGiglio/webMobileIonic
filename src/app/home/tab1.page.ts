079ec00c00caa3b1f3ce53da695b5f123842362a
Alexandre BG <alexandre.brungiglio@nextalys.com>
2021-11-05 15:57:17 +0100

    data local storage

--- src/app/home/tab1.page.ts
+++ src/app/home/tab1.page.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
+import { languageKey } from '../../environments/constant';
 import { RoutesList } from '../../environments/routes';
 
+export interface Language {
+  name: string;
+  order: number;
+  imgUrl: string;
+}
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,8 +14,17 @@ import { RoutesList } from '../../environments/routes';
 })
 export class Tab1Page {
   routes = RoutesList;
+  languageList: Language[] = [];
 
-  constructor() { }
+  constructor() {
+    this.loadData();
+  }
+
+  loadData() {
+    const jsonLanguageList: Language[] = JSON.parse(localStorage.getItem(languageKey));
+    this.languageList = jsonLanguageList.sort((a, b) => a.order - b.order);
+    console.log('🚀 ~ Tab1Page ~ loadData ~ this.languageList', this.languageList);
+  }
 
   editProperty() {
     console.log('Echo');
